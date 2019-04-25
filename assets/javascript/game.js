737dac0b0a48527e1a0e22a1acb37a5bc992f8d1
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-04-25 16:18:19 -0500

    refined app

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,23 @@
 //List of Possible Answers
-var wordPool = ["philip johnson","antoni gaudi", "alvar aalto","eero saarinen","louis kahn","zaha hadid"];
-var imgLink = ["Philip_Johnson.jpg","Antoni_Gaudi.jpg","Alvar_Aalto.jpg","eero_saarinen.jpg","Louis_Kahn.jpg","Zaha_Hadid.jpg"]
+var wordPool = ["philip johnson","antoni gaudi", "alvar aalto","eero saarinen","louis kahn","zaha hadid", "buckminster fuller", "frank gehry", "frank lloyd wright", "le corbusier", "mies van der rohe", "rem koolhaas", "renzo piano"];
+var imgLink = ["Philip_Johnson.jpg","Antoni_Gaudi.jpg","Alvar_Aalto.jpg","eero_saarinen.jpg","Louis_Kahn.jpg","Zaha_Hadid.jpg", "Buckminster_Fuller.jpg", "Frank_Gehry.jpg", "Frank_Lloyd_Wright.jpg", "Le_Corbusier.jpg", "Mies_Van_Der_Rohe.jpg", "Rem_Koolhaas.jpg", "Renzo_Piano.png"]
 //Randomized Selection
 var selector = Math.floor((Math.random() * wordPool.length));
 var word = wordPool[selector];
 var linkImg = "assets/images/" + imgLink[selector];
 var wordLength = word.length;
+var targetNumber = word.length;
 var lives = 10;
 var tally = 0;
 var rejectedLetters = [];
-var endGame = false
+var acceptedLetters = [];
+var endGame = false;
 // console.log(word);
 // console.log(wordLength);
 
-var fxn = function (tally, lives){    
-    while ( tally === wordLength && endGame === false){
+var fxn = function (){   
+    //win 
+    while ( tally === targetNumber && endGame === false){
         endGame = true;
         // console.log("tally = " + tally + "!")
         tally = 0;
@@ -30,9 +33,17 @@ var fxn = function (tally, lives){
         pic.setAttribute("alt", "Architecture Picture");
         display.appendChild(result);
         display.appendChild(pic);
+        //replace instructiondiv with button????
+        var iDiv = document.getElementById('instructionDiv');
+        var restartButton = document.createElement("button")
+        iDiv.innerHTML = "";
+        restartButton.innerHTML = "RESET";
+        restartButton.setAttribute("id", "restartButton")
+        restartButton.onclick = resetPage
+        iDiv.append(restartButton)
         return tally;
     }
-    
+    //lose
     while ( lives < 1 ){
         lives = 10;
         tally = 0;
@@ -48,7 +59,12 @@ var fxn = function (tally, lives){
 for( i = 0 ; i < wordLength; i++){
     var gDiv = document.getElementById("gameDiv");
     var letterBox = document.createElement("div");
-    var placeHolder = document.createTextNode("_");
+    if(word.charAt([i]) === " "){
+        var placeHolder = document.createTextNode(" ");
+        targetNumber--;
+    } else {
+        var placeHolder = document.createTextNode("_");
+    }
     letterBox.id = "LB" + [i];
     letterBox.classList.add("letterBoxes");
     letterBox.appendChild(placeHolder);
@@ -57,63 +73,93 @@ for( i = 0 ; i < wordLength; i++){
 
 //Keypress Listener
 document.addEventListener("keydown", function(e){
-    console.log("you pressed: " + e.key);
+    // console.log("you pressed: " + e.key);
     var match = false;
-    //for loop
-    for( i = 0; i < wordLength; i++){
-        var mark = word.charAt([i]);
-        var shortWordLength = wordLength - 1;
-        var key = e.key;
-        console.log(mark);
+    if(!findRepeat(e.key)){
+        //for loop
+        for( i = 0; i < wordLength; i++){
+            var mark = word.charAt([i]);
+            var shortWordLength = wordLength - 1;
+            var key = e.key;
+            console.log(mark);
 
-            if ( ([i] < shortWordLength) && (e.key === mark) ){
-                var transform = document.getElementById("LB" + [i]);
-                match = true;
-                tally++;
-                transform.innerHTML = mark;
-                console.log("looks great!");
-                fxn(tally, lives);
-            }
+                //not finished looking, but found a match
+                if ( ([i] < shortWordLength) && (e.key === mark) ){
+                    var transform = document.getElementById("LB" + [i]);
+                    match = true;
+                    acceptedLetters.push(e.key)
+                    tally++;
+                    transform.innerHTML = mark.toUpperCase();
+                    console.log("looks great!");
+                    fxn();
+                }
+                //not finished looking, but no match yet
+                else if ( ( [i] < shortWordLength) && (e.key != mark || match == false) ){
+                    console.log("Nope. Keep looking.");
+                    fxn();
+                }
 
-            else if ( ( [i] < shortWordLength) && (e.key != mark || match == false) ){
-                console.log("Nope. Keep looking.");
-                fxn(tally, lives);
-            }
+                //found matching last letter
+                else if ( ( [i] == shortWordLength) && (e.key === mark) ){
+                    var transform = document.getElementById("LB" + [i]);
+                    match = true;
+                    acceptedLetters.push(e.key)
+                    tally++;
+                    transform.innerHTML = mark.toUpperCase();
+                    console.log("looks great(final)!");
+                    fxn();
+                    return match;
+                }
 
-            else if ( ( [i] == shortWordLength) && (e.key === mark) ){
-                var transform = document.getElementById("LB" + [i]);
-                match = true;
-                tally++;
-                transform.innerHTML = mark;
-                console.log("looks great(final)!");
-                fxn(tally, lives);
-                return match;
-            }
+                //no matching last letter
+                else if( ([i] == shortWordLength) && (match === false) ) {
+                    var sDiv = document.getElementById("tries");
+                    lives--;
+                    var triesMessage = "Tries left: " + lives;
+                    var rejLetters= document.getElementById("rejects");
+                    var RLBox = document.createElement("div");
+                    sDiv.innerHTML = triesMessage;
+                    rejectedLetters.push(key)
+                    RLBox.innerHTML= e.key;
+                    RLBox.classList.add("RLetterBoxes");
+                    rejLetters.appendChild(RLBox);
+                    console.log("no match found");
+                    console.log(rejectedLetters)
+                    fxn();
+                    break;
+                }
 
-            else if( ([i] == shortWordLength) && (match === false) ) {
-                var sDiv = document.getElementById("tries");
-                lives--;
-                var triesMessage = "Tries left: " + lives;
-                var rejLetters= document.getElementById("rejects");
-                var RLBox = document.createElement("div");
-                sDiv.innerHTML = triesMessage;
-                rejectedLetters.push(key)
-                RLBox.innerHTML= e.key;
-                RLBox.classList.add("RLetterBoxes");
-                rejLetters.appendChild(RLBox);
-                console.log("no match found");
-                console.log(rejectedLetters)
-                fxn(tally, lives);
-                break;
-            }
-
-           //inside forloop 
+            //inside forloop 
         }
         //outside forloop
-    
+        console.log("tally = " + tally)
+        console.log("targetNumber = " + targetNumber);
+    }
 });
 
-console.log("tally = " + tally);
+function findRepeat(key){
+    var repeat = false;
+    for(var i = 0; i < rejectedLetters.length; i++){
+        if(key === rejectedLetters[i]){
+            repeat = true;
+        }
+    }
+    for(var i = 0; i < acceptedLetters.length; i++){
+        if(key === acceptedLetters[i]){
+            repeat = true;
+        }
+    }
+    console.log("repeat = " + repeat)
+    return repeat;
+}
+
+// document.getElementById('restartButton').onclick(resetPage())
+
+function resetPage(){
+    window.location.reload()
+}
+
+console.log("targetNumber = " + targetNumber);
 
 /*  1)losing conditions not triggering
     2)no duplicates in reject letter array
